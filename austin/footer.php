5717b240f2fb3635144431a372989179d84f4d9d
austinsirkin <austin.sirkin@gmail.com>
2018-08-20 15:53:08 -0400

    Update footer.php
    Updating for PSR-1 standards.

--- austin/footer.php
+++ austin/footer.php
@@ -1,8 +1,13 @@
- <footer><table style="width: 40%"><tr><td><center>&copy;Austin Sirkin. Last modified on 
+<footer>
+<table style="width: 40%">
+<tr>
+<td><center>&copy;Austin Sirkin. Last modified on 
+
 <?php 
-
-  // Gotta use that getlastmod function for the footer! Everyone knows that!
-
 echo date("F d, Y \a\\t h:i:s a e", getlastmod()); 
-    
-    ?></center></td></tr></table></footer>
+?>
+
+</center></td>
+</tr>
+</table>
+</footer>
